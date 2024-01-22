04b87ecf00e713820e2311ec5930dfa8d2855464
karl <karltacheron@gmail.com>
2024-01-22 15:58:08 -0500

    quick-build function

--- lua/build.lua
+++ lua/build.lua
@@ -1,22 +1,28 @@
+
 function build()
-    -- Get the directory of the current buffer
     local buf_path = vim.fn.expand('%:p:h')
 
-    -- Build the full path to the build script
-    local build_script = buf_path .. "/build.sh"
-
-    -- Check if the build script exists
-    if vim.fn.filereadable(build_script) == 1 then
-        -- Change to the buffer's directory
-        vim.cmd("lcd " .. buf_path)
-
-        -- Run the build script
-        vim.api.nvim_command(':w | !./build.sh')
-
-        -- Return to the original directory
-        vim.cmd("lcd -")
+    if vim.bo.filetype == "python" then
+        vim.api.nvim_command(':w | !python3 %')
     else
-        print("Build script not found!")
+       -- Get the directory of the current buffer
+   
+       -- Build the full path to the build script
+       local build_script = buf_path .. "/build.sh"
+   
+       -- Check if the build script exists
+       if vim.fn.filereadable(build_script) == 1 then
+           -- Change to the buffer's directory
+           vim.cmd("lcd " .. buf_path)
+   
+           -- Run the build script
+           vim.api.nvim_command(':w | !./build.sh')
+   
+           -- Return to the original directory
+           vim.cmd("lcd -")
+       else
+           print("Build script not found!")
+       end
     end
 end
 

aa8f5189528b0ba53495e0a34529ad8745b3262c
karl <karltacheron@gmail.com>
2024-01-23 09:45:35 -0500

    plugin settings to another file

---
+++ lua/pluginsettings.lua
@@ -0,0 +1,69 @@
+vim.cmd [[ colorscheme NeoSolarized ]]
+vim.cmd [[ set background=light ]]
+
+local telescope = require('telescope')
+-- the extensions are not setup anymore
+telescope.setup({
+    -- ...
+    extensions = {
+        heading = {
+	    picker_opts = {sorting_strategy = 'ascending'},
+            treesitter = true,
+        },
+    },
+})
+
+require('scrollview').setup({
+  excluded_filetypes = {'nerdtree'},
+  current_only = true,
+  base = 'right',
+  column = 2,
+  -- scrollview_signs_column = 1,
+  signs_column=1,
+  signs_overflow =1,
+  signs_on_startup = {'all'},
+  diagnostics_severities = {vim.diagnostic.severity.ERROR},
+})
+
+require("auto-save").setup({
+  enable = true,
+    condition = function(buf)
+      -- only run for gitted buffers
+      if isbufgitted() then
+        return true
+      end
+      return false
+    end
+})
+
+
+-- Was using this more when my setup was unreliable and i was tired of opening the same buffers a lot
+-- require("auto-session").setup {
+--   auto_session_enabled = true,
+--   auto_restore_enabled = true,
+--   log_level = "error",
+--   auto_session_root_dir = '~/.config/nvim/sessions/',
+--       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+-- }
+
+
+-- telescope.load_extension('heading')
+-- -- telescope
+-- require('telescope').load_extension('heading')
+
+-- -- autocomplete
+-- vim.cmd [[set completeopt=menu,noinsert,noselect]]
+-- 
+-- vimtex
+-- vim.g.vimtex_compiler_method = 'tectonic'
+-- 
+-- -- treesitter
+-- require('nvim-treesitter.configs').setup({
+--     ensure_installed = {
+--         -- ..
+--         'markdown',
+--         'rst',
+-- 	'latex'
+--         -- ..
+--     },
+-- })
